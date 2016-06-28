5f59651136bd33580150295b1e2e560b5658f54e
adajic1 <adajic1@etf.unsa.ba>
2016-06-28 16:59:43 +0200

    Memory leak fixed
    Fixed memory leak, faster loading, won't download files with the same id
    more than once, code better commented etc...

--- download.js
+++ download.js
@@ -1,35 +1,33 @@
 // SYTNTAX
-// casperjs --load-images=no --ignore-ssl-errors=yes download.js <id_of_subject> [<username> <password>]
+// casperjs --ignore-ssl-errors=yes download.js <id_of_subject> [<username> <password>]
 // EXAMPLE CALL TO DOWNLOAD TP
-// casperjs --load-images=no --ignore-ssl-errors=yes download.js 76
+// casperjs --ignore-ssl-errors=yes download.js 76
 /* ############################################################ */
 /* ################### DEVELOPED BY Ice_VII ################### */
 /* ############################################################ */
 
-var startTime=Date.now();
-
-begUrl="http://c2.etf.unsa.ba";
-var fs = require('fs');
-var page = require('webpage').create();
-var startTime=Date.now();
-var endTime;
-var loaded=0;
-var system = require('system');
+var startTime=Date.now(); // Start of the program
+var begUrl="http://c2.etf.unsa.ba"; // First part of urls
+var fs = require('fs'); // To work with files
+var casperReq = require('casper');
+var page = require('webpage').create(); // To use phantomjs page object
+var loaded=0; // State variable, counting stages
+var system = require('system'); // To get access to console arguments
 var args = system.args;
-var id_predmeta="";
-var login_data=false;
-var username="";
+var subjectId=""; // Id of subject will come as command line argument
+var loginProvided=false; // Login details provided or not
+var username=""; // Credentials
 var pass="";
-var idevi=[];
-var fold=[];
-var folder="";
-var stariFolder="";
-var linkFile="";
-var filename="";
-var old_filename="";
-var skiniResurs=true;
-var naslov="";
-var tr=-1;
+var idevi=[]; // Array of ids to be downloaded
+var fold=[]; // Array with folder names in form of sedmica_<WEEK>
+var folder=""; // Subject name + / + fold
+var oldFolder=""; // Console log whenever folder changes
+var linkFile=""; // Full url link to a resource file
+var filename=""; // Name of file extracted from the url
+var resurs; // Is the current link a file resource or not
+var done; // Checking resourceRequests only while done==false
+var subjectTitle=""; // Will read it from the <title> tag from the website
+var tr=-1; // Index of the id currently being processed
 
 console.log("------------------------------");
 console.log("AUTOMATIC C2 COURSE DOWNLOADER");
@@ -37,41 +35,42 @@ console.log("----------BY ICE_VII----------");
 console.log("------------------------------");
 
 if (args.length <= 4) {
-  console.log('Syntax: casperjs --load-images=no --ignore-ssl-errors=yes download.js <id_of_subject> [<username> <password>]');
+  console.log('Syntax: casperjs --ignore-ssl-errors=yes download.js <id_of_subject> [<username> <password>]');
   phantom.exit();
 } else {
-	id_predmeta=args[4];
+	subjectId=args[4];
 	if (args.length>5) {
 		username=args[5];
-		login_data=true;
+		loginProvided=true;
 		if (args.length>6) 
 			pass=args[6];
 		else {
-			console.log('Syntax: casperjs --ignore-ssl-errors=yes --load-images=no down.js <id_of_subject> [<username> <password>]');
+			console.log('Syntax: casperjs --ignore-ssl-errors=yes download.js <id_of_subject> [<username> <password>]');
 			phantom.exit();
 		}
 	}
 }
-var url = begUrl+"/course/view.php?id="+id_predmeta;
+var url = begUrl+"/course/view.php?id="+subjectId;
 
 function getFilename(str) {
     return str.split('\\').pop().split('/').pop();
 }
+
 page.open(url);			
 page.onLoadFinished = function(status) {
 	if (status === 'success') { // The website is opened successfully
 		if (loaded==0) {
 			loaded=1;
 			console.log("The login website was loaded successfully");
-			page.evaluate(function(login_data,username,pass) {
-				if (!login_data) {
+			page.evaluate(function(loginProvided,username,pass) {
+				if (!loginProvided) {
 					document.getElementById("guestlogin").submit();
 				} else {
 					document.getElementById("username").value=username;
 					document.getElementById("password").value=pass;
 					document.getElementById("login").submit();
 				}				
-			},login_data,username,pass); 
+			},loginProvided,username,pass); 
 		} else if (loaded==1) {
 			loaded=2;
 			uspjelo=page.evaluate(function() {
@@ -81,9 +80,9 @@ page.onLoadFinished = function(status) {
 				console.log("Wrong username or password");
 				phantom.exit();
 			}
-			if (!login_data) console.log("Guest login success");
+			if (!loginProvided) console.log("Guest login success");
 			else console.log("Login success");
-			page.open(begUrl+"/course/view.php?id="+id_predmeta,function(status){
+			page.open(begUrl+"/course/view.php?id="+subjectId,function(status){
 				page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js",function() {
 					if (status === 'success') { // The website is opened successfully
 						if (loaded==2) {
@@ -108,33 +107,35 @@ page.onLoadFinished = function(status) {
 								});								
 								return vrati;
 							},begUrl);	
-							naslov=page.evaluate(function() {
+							subjectTitle=page.evaluate(function() {
 								return $('title').text();
 							});
 							poz=0;
-							new_naslov="";
-							while (poz<naslov.length && naslov.charAt(poz)==' ') poz++;
-							while (poz<naslov.length) {
-								if (naslov.charAt(poz)!=':' && naslov.charAt(poz)!=';')
-									new_naslov+=naslov.charAt(poz);
+							var new_subjectTitle="";
+							while (poz<subjectTitle.length && subjectTitle.charAt(poz)==' ') poz++;
+							while (poz<subjectTitle.length) {
+								if (subjectTitle.charAt(poz)!=':' && subjectTitle.charAt(poz)!=';')
+									new_subjectTitle+=subjectTitle.charAt(poz);
 								poz++;
 							}
-							naslov=new_naslov;
-							console.log(naslov);
-							if (!fs.exists(naslov))
-								fs.makeDirectory(naslov);
+							subjectTitle=new_subjectTitle;
+							console.log(subjectTitle);
+							if (!fs.exists(subjectTitle))
+								fs.makeDirectory(subjectTitle);
 							page.zoomFactor=1;
-							page.render(naslov+'/Browser_screenshot.png');
+							page.render(subjectTitle+'/Browser_screenshot.png');
 							console.log("Browser_screenshot.png has been generated");	
 							folderi=podaci.split("\n");
 							for (var i=0; i<folderi.length-1; i++) {
-								ids=folderi[i].split(" ");
+								var ids=folderi[i].split(" ");
 								for (var j=0; j<ids.length-1; j++) {
-									idevi.push(ids[j]);
-									fold.push("sedmica_"+i.toString());
+									if (idevi.indexOf(ids[j])==-1) {
+										idevi.push(ids[j]);
+										fold.push("sedmica_"+i.toString());
+									}
 								}
 							}			
-							download();									
+							setTimeout(function(){delete page; download();},1);									
 						}
 					}
 				});
@@ -148,50 +149,52 @@ page.onLoadFinished = function(status) {
 };
 
 function onResourceRequested(requestData, request) {
-	if (requestData.url.indexOf(begUrl+"/file.php/")!=-1) {
+	if (!done && requestData.url.indexOf(begUrl+"/file.php/")!=-1) {
 		linkFile=requestData.url;	
 		filename=getFilename(linkFile);
-		if (filename==old_filename)
-			skiniResurs=false;
-		else 
-			skiniResurs=true;
-		old_filename=filename;
+		resurs=true;
+		done=true;
 	}
 }
 	
 function download() {
 	tr++;
 	if (tr==idevi.length) {
-		endTime=Date.now();
-		difference=(endTime-startTime)*1.0/1000;
+		var endTime=Date.now();
+		var difference=(endTime-startTime)*1.0/1000;
 		console.log("FINISH");
 		console.log("Total download time: "+difference.toString()+" secs");
 		if (idevi.length==0) {
-			message="\nNOTES:\nThe process finished with zero downloaded files? Try to use your credentials instead of the guest login syntax. ";
+			var message="\nNOTES:\nThe process finished with zero downloaded files? Try to use your credentials instead of the guest login syntax. ";
 			message+="\nIt finished with zero downloaded files AGAIN? Enroll yourself manually on the course, then try again. ";
-			message+="\nCheck the image "+naslov+"/Browser_screenshot.png to get an idea what's happening in the headless webkit browser.";
+			message+="\nCheck the image "+subjectTitle+"/Browser_screenshot.png to get an idea what's happening in the headless webkit browser.";
 			console.log(message);
 		}
 		phantom.exit();
 	}
-	downlink=begUrl+"/mod/resource/view.php?id="+idevi[tr];
-	folder=naslov+"/"+fold[tr];
-	if (folder!=stariFolder) {
+	var downlink=begUrl+"/mod/resource/view.php?inpopup=true&id="+idevi[tr];
+	folder=subjectTitle+"/"+fold[tr];
+	if (folder!=oldFolder) {
 		console.log("Folder: "+fold[tr]);
-		stariFolder=folder;
+		oldFolder=folder;
 	}
 	if (!fs.exists(folder))
 		fs.makeDirectory(folder);
-	var casper=require('casper').create({
+	done=false;
+	resurs=false;
+	casper=casperReq.create({
 		pageSettings: {
-			webSecurityEnabled: false
+			webSecurityEnabled: false,
+			loadImages : true,
+			loadPlugins : false,
+			userAgent: 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0'
 		}
 	});
-	casper.start(begUrl+"/mod/resource/view.php");	
-	casper.thenOpen(begUrl+"/mod/resource/view.php?inpopup=true&id="+idevi[tr]);	
-	casper.on('resource.requested', onResourceRequested);
+	casper.on('resource.requested', onResourceRequested); // Listening only while done==false
+	casper.start(downlink);	
 	casper.then(function(){
-		if (skiniResurs) {
+		done=true;
+		if (resurs) {
 			console.log("    "+(tr+1).toString()+"/"+idevi.length+" Saving "+filename+" id="+idevi[tr]);
 			this.download(linkFile, folder+"/"+filename);
 		} else {
@@ -203,7 +206,11 @@ function download() {
 		}
 	});
 	casper.run(function() {
-		download();
+		setTimeout(function(){
+			casper.page.close();
+			casper.page = require('webpage').create();
+			download();
+		},1);
 	});	
 }
 
